2ee00b1f425648b226d0b323c79af60c15dd2b8d
Pedro Vitor <pedrovtor8@gmail.com>
2025-04-24 01:40:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
