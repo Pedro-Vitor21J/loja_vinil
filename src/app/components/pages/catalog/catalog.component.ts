037bdbe205fe73da50d506fac70501d5006dd588
Pedro Vitor <pedrovtor8@gmail.com>
2025-04-29 00:55:01 -0300

    Feat(footer): Cria footer

--- src/app/components/pages/catalog/catalog.component.ts
+++ src/app/components/pages/catalog/catalog.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "../../template/header/header.component";
 import { VinlycardComponent } from "../../template/vinlycard/vinlycard.component";
 import { vinly } from '../../model/vinly';
 import { vinly_mock } from '../../../../mock/mocks';
+import { FooterComponent } from "../../template/footer/footer.component";
 
 
 
 @Component({
   selector: 'app-catalog',
-  imports: [HeaderComponent, VinlycardComponent],
+  imports: [HeaderComponent, VinlycardComponent, FooterComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
