d3bd7680bb8105fe42f43a4d9a98305d92c95126
Pedro Vitor <pedrovtor8@gmail.com>
2025-04-30 23:22:48 -0300

    Feat(routes): Cria rotas e atualiza o CSS do filters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './components/pages/catalog/catalog.component';
 import { DetailComponent } from './components/pages/detail/detail.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-    /*{path: "" , component: CatalogComponent}*/
-    {path: "home", component: DetailComponent}
+    {path: "", component: HomeComponent},
+    {path: "catalog", component: CatalogComponent},
+    {path: "detail", component: DetailComponent}
 ];
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../template/header/header.component";
+import { FooterComponent } from "../../template/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
