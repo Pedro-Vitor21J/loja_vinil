b2b0300b7a4ab9d12d8b31784b676b0ef8c99f05
Pedro Vitor <pedrovtor8@gmail.com>
2025-05-21 03:07:54 -0300

    Feat(home): Atualiza a home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../template/header/header.component";
 import { FooterComponent } from "../../template/footer/footer.component";
+import { BannerComponent } from "../../template/banner/banner.component";
+import { CarouselComponent } from "../../template/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BannerComponent, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
